c5bfcc16e3b89977fe915690288af9343a6bb3de
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2023-02-21 20:49:00 +0530

    Local user state

--- App.js
+++ App.js
@@ -1,33 +1,82 @@
 // @refresh state
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import * as firebase from "firebase";
-import "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
+import {
+  StyleSheet,
+  Button,
+  Text,
+  TextInput,
+  View,
+  YellowBox,
+} from "react-native";
+// import * as firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCh_TcQAZ9kX88gaU1IPazRo8MNqdyQYp4",
-  authDomain: "react-native-chat-8df84.firebaseapp.com",
-  projectId: "react-native-chat-8df84",
-  storageBucket: "react-native-chat-8df84.appspot.com",
-  messagingSenderId: "95593239987",
-  appId: "1:95593239987:web:01fdfd9015c9ae734067c6",
+  apiKey: "AIzaSyBOK7x5N5UnjY4TDqndzH7l5tvdNIsWFRc",
+  authDomain: "todo-app-e3cf0.firebaseapp.com",
+  projectId: "todo-app-e3cf0",
+  storageBucket: "todo-app-e3cf0.appspot.com",
+  messagingSenderId: "940016886081",
+  appId: "1:940016886081:web:91686613f16b1b1f8001c0",
+  measurementId: "G-JHPC7TP12K",
 };
 
+// Use this to initialize the firebase App
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// Use these for db & auth
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
 if (firebase.apps.length === 0) {
   const app = initializeApp(firebaseConfig);
 }
 // Initialize Firebase
 
+YellowBox.ignoreWarnings(["Setting a timer"]);
+
 export default function App() {
+  const [user, setUser] = useState(null);
+  const [name, setName] = useState("");
+  useEffect(() => {
+    readUser();
+  }, []);
+
+  async function readUser() {
+    const user = await AsyncStorage.getItem("user");
+    if (user) {
+      setUser(JSON.parse(user));
+    }
+  }
+
+  async function handlePress() {
+    const _id = Math.random().toString(36).substring(7);
+    const user = { _id, name };
+    await AsyncStorage.setItem("user", JSON.stringify(user));
+    setUser(user);
+  }
+
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your name"
+          value={name}
+          onChangeText={setName}
+        />
+        <Button title="Enter the Chat" onPress={handlePress} />
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>We have an user</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -39,5 +88,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 30,
+  },
+  input: {
+    height: 50,
+    width: "100%",
+    borderWidth: 1,
+    padding: 15,
+    borderColor: "gray",
+    marginButtom: 20,
   },
 });
